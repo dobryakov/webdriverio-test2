a78eee2830f70437a6686c12aa861d411a083462
Grigoriy Dobryakov <gdobryakov@bekitzur.com>
2019-08-01 18:34:13 +0300

    prepare data for testrail api (in progress)

--- components/custom_reporter.ts
+++ components/custom_reporter.ts
@@ -1,15 +1,50 @@
 import WDIOReporter from '@wdio/reporter';
 
 export default class CustomReporter extends WDIOReporter {
+
+    results:Array<any>;
+
     constructor (options) {
         /**
          * make reporter to write to output stream by default
          */
         options = Object.assign(options, { stdout: true });
         super(options);
+        this.results = [];
     }
 
-    onTestPass (test) {
+    onTestPass(test) {
+        //console.log(test);
         console.log(`Congratulations! Your test "${test.title}" passed 👏`);
+        this.results.push(test);
+    }
+
+    onTestFail(test) {
+        //console.log(test);
+        console.log(`Your test "${test.title}" failed 👏`);
+        this.results.push(test);
+    }
+
+    onSuiteEnd() {
+        //console.log(this.results);
+        console.log({results: this.results.map(function(result, index, arr){
+
+            let comment   = '';
+
+            let status_id = null;
+            if (result.state == 'passed') { status_id = 1 }
+            if (result.state == 'failed') {
+                status_id = 5;
+                comment   = result.error.message;
+            }
+
+            return {
+                // todo refactor it to catch multiple test cases in one test
+                case_id:   result.title.match(/C\d/g)[0].match(/\d/g)[0],
+                status_id: status_id,
+                comment:   comment,
+            }
+
+        })});
     }
 };
--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,9 +1,18 @@
 const assert = require('assert');
 
 describe('webdriver.io page', () => {
-    it('should have the right title', () => {
+
+    it('C4 - should have the right title', () => {
         browser.url('https://webdriver.io');
         const title = browser.getTitle();
         assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
     });
+
+    it('C7 - should fails', () => {
+        //browser.url('https://ya.ru');
+        //const title = browser.getTitle();
+        //assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+        assert.strictEqual('a', 'b');
+    });
+
 });
